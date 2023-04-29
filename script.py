31ac38513a5c0909c8f9fb3ab1585411b4b7f95e
Jason Pavlopoulos <Jasonpavlop1@gmail.com>
2023-04-29 19:12:19 +0300

    First Step Completed

--- script.py
+++ script.py
@@ -114,7 +114,7 @@ class App(customtkinter.CTk):
             height=(40),
             width=(300),
             font=customtkinter.CTkFont(size=18, weight="bold"),
-            command=self.first_figure,
+            command=self.eighth_figure,
         )
         self.sidebar_button_8.grid(row=8, column=0, padx=20, pady=10)
         self.sidebar_button_9 = customtkinter.CTkButton(
@@ -150,7 +150,7 @@ class App(customtkinter.CTk):
         )  # Create a new column for short month names
         monthly_data = (
             data.groupby(["Year", "Month", "Measure"])["Value"].sum().reset_index()
-        )  # Group the data by year and month, and sum the Value column for $ and Tonnes measures separately
+        )  # Group the data by Year, Month and Measure and sum the Value column for $ and Tonnes measures separately
 
         month_order = [
             "Jan",
@@ -179,19 +179,22 @@ class App(customtkinter.CTk):
         )
 
         fig, axs = plt.subplots(
-            nrows=2, figsize=(9.39, 6.48)
+            num="Profit per Month", nrows=2, figsize=(9.39, 6.48)
         )  # Create the bar charts as subplots
-        dollars_data.plot(kind="bar", ax=axs[0], legend=False)  # Plot the graph
+
+        # Plot the graphs
+        dollars_data.plot(kind="bar", ax=axs[0], legend=False)
         axs[0].set_title("Monthly Value by Year ($)")
         axs[0].set_xlabel("Month")
         axs[0].set_ylabel("Value ($)")
 
         fig.legend(loc="upper right", ncol=len(month_order))
-        tonnes_data.plot(kind="bar", ax=axs[1], legend=False)  # Plot the graph
+        tonnes_data.plot(kind="bar", ax=axs[1], legend=False)
         axs[1].set_title("Monthly Value by Year (Tonnes)")
         axs[1].set_xlabel("Month")
         axs[1].set_ylabel("Value (Tonnes)")
 
+        fig.canvas.manager.set_window_title("Profit per Month")
         mngr = plt.get_current_fig_manager()
         mngr.window.geometry("+661+210")
         plt.tight_layout(pad=3.0)
@@ -213,7 +216,7 @@ class App(customtkinter.CTk):
         ]  # Accept only the 'Exports' Direction
         data = (
             data.groupby(["Country", "Measure"])["Value"].sum().reset_index()
-        )  # Group the data by country, measure, year, and month, and sum the Value column for $ and Tonnes measures separately
+        )  # Group the Value by Country and Measure and sum for $ and Tonnes measures separately
         data = data.replace(
             [
                 "East Asia (excluding China)",
@@ -233,18 +236,20 @@ class App(customtkinter.CTk):
             index="Country", values="Value", aggfunc="sum"
         )
 
-        axs = plt.subplots(
-            nrows=2, figsize=(9.39, 6.48)
+        fig, axs = plt.subplots(
+            num="Profit per Country", nrows=2, figsize=(9.39, 6.48)
         )  # Create the bar charts as subplots
-        dollars_data.plot(kind="bar", ax=axs[0], legend=False)  # Plot the graph
+        # Plot the graphs
+        dollars_data.plot(kind="bar", ax=axs[0], legend=False)
         axs[0].set_title("Summary Value by Country ($)")
         axs[0].set_xlabel("Country")
         axs[0].set_ylabel("Value ($)")
-        tonnes_data.plot(kind="bar", ax=axs[1], legend=False)  # Plot the graph
+        tonnes_data.plot(kind="bar", ax=axs[1], legend=False)
         axs[1].set_title("Summary Value by Country (Tonnes)")
         axs[1].set_xlabel("Country")
         axs[1].set_ylabel("Value (Tonnes)")
 
+        fig.suptitle("Profit Per Country", fontweight="bold", fontsize=16)
         mngr = plt.get_current_fig_manager()
         mngr.window.geometry("+661+210")
         plt.tight_layout()
@@ -266,7 +271,7 @@ class App(customtkinter.CTk):
         ]  # Accept only the 'Exports' Direction
         data = (
             data.groupby(["Transport_Mode", "Measure"])["Value"].sum().reset_index()
-        )  # Group the data by Transport_Mode and measure and sum the Value column for $ and Tonnes measures separately
+        )  # Group the data by Transport_Mode and Measure and sum the Value column for $ and Tonnes measures separately
 
         # Create pivot tables with Transport_Mode as rows, and the sum of values as values for $ and Tonnes measures separately
         dollars_data = data[data["Measure"] == "$"].pivot_table(
@@ -276,18 +281,20 @@ class App(customtkinter.CTk):
             index="Transport_Mode", values="Value", aggfunc="sum"
         )
 
-        axs = plt.subplots(
-            nrows=2, figsize=(9.39, 6.48)
+        fig, axs = plt.subplots(
+            num="Profit per Transport", nrows=2, figsize=(9.39, 6.48)
         )  # Create the bar charts as subplots
-        dollars_data.plot(kind="bar", ax=axs[0], legend=False)  # Plot the graph
+        # Plot the graphS
+        dollars_data.plot(kind="bar", ax=axs[0], legend=False)
         axs[0].set_title("Summary Value by Transport Mode ($)")
         axs[0].set_xlabel("Transport")
         axs[0].set_ylabel("Value ($)")
-        tonnes_data.plot(kind="bar", ax=axs[1], legend=False)  # Plot the graph
+        tonnes_data.plot(kind="bar", ax=axs[1], legend=False)
         axs[1].set_title("Summary Value by Transport Mode (Tonnes)")
         axs[1].set_xlabel("Transport")
         axs[1].set_ylabel("Value (Tonnes)")
 
+        fig.canvas.manager.set_window_title("Profit per Transport")
         mngr = plt.get_current_fig_manager()
         mngr.window.geometry("+661+210")
         plt.tight_layout()
@@ -312,14 +319,14 @@ class App(customtkinter.CTk):
         )  # Create a new column for short day names
         daily_data = (
             data.groupby(["Day", "Measure"])["Value"].sum().reset_index()
-        )  # Group the data by Day and Measure, and sum the Value column for $ and Tonnes measures separately
+        )  # Group the data by Day and Measure and sum the Value column for $ and Tonnes measures separately
 
         month_order = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]  # Days Order
         daily_data["Day"] = pd.Categorical(
             daily_data["Day"], categories=month_order, ordered=True
         )  # Ordering the Columns By Days
 
-        # Create pivot tables Day as rows, and the sum of values as values for $ and Tonnes measures separately
+        # Create pivot tables with Day as rows, and the sum of values as values for $ and Tonnes measures separately
         dollars_data = daily_data[daily_data["Measure"] == "$"].pivot_table(
             index="Day", values="Value", aggfunc="sum"
         )
@@ -327,15 +334,16 @@ class App(customtkinter.CTk):
             index="Day", values="Value", aggfunc="sum"
         )
 
-        axs = plt.subplots(
-            nrows=2, figsize=(9.39, 6.48)
+        fig, axs = plt.subplots(
+            num="Profit per Day of Week", nrows=2, figsize=(9.39, 6.48)
         )  # Create the bar charts as subplots
-        dollars_data.plot(kind="bar", ax=axs[0], legend=False)  # Plot the graph
+        # Plot the graphs
+        dollars_data.plot(kind="bar", ax=axs[0], legend=False)
         axs[0].set_title("Total Daily Value ($)")
         axs[0].set_xlabel("Day")
         axs[0].set_ylabel("Value ($)")
 
-        tonnes_data.plot(kind="bar", ax=axs[1], legend=False)  # Plot the graph
+        tonnes_data.plot(kind="bar", ax=axs[1], legend=False)
         axs[1].set_title("Total Daily Value (Tonnes)")
         axs[1].set_xlabel("Day")
         axs[1].set_ylabel("Value (Tonnes)")
@@ -361,7 +369,7 @@ class App(customtkinter.CTk):
         ]  # Accept only the 'Exports' Direction
         data = (
             data.groupby(["Commodity", "Measure"])["Value"].sum().reset_index()
-        )  # Group the data by Commodity and Measure, and sum the Value column for $ and Tonnes measures separately
+        )  # Group the data by Commodity and Measure and sum the Value column for $ and Tonnes measures separately
 
         # Replace the Long names of the Commodity Values
         data = data.replace(
@@ -385,7 +393,7 @@ class App(customtkinter.CTk):
             ],
         )
 
-        # Create pivot tables Commodity as rows, and the sum of values as values for $ and Tonnes measures separately
+        # Create pivot tables Commodity as rows and the sum of values as values for $ and Tonnes measures separately
         dollars_data = data[data["Measure"] == "$"].pivot_table(
             index="Commodity", values="Value", aggfunc="sum"
         )
@@ -393,17 +401,18 @@ class App(customtkinter.CTk):
             index="Commodity", values="Value", aggfunc="sum"
         )
 
-        axs = plt.subplots(
-            nrows=2, figsize=(9.39, 6.48)
+        fig, axs = plt.subplots(
+            num="Profit per Product", nrows=2, figsize=(9.39, 6.48)
         )  # Create the bar charts as subplots
-        dollars_data.plot(kind="bar", ax=axs[0], legend=False)  # Plot the graph
-        axs[0].set_title("Total Daily Value ($)")
-        axs[0].set_xlabel("Day")
+        # Plot the graphs
+        dollars_data.plot(kind="bar", ax=axs[0], legend=False)
+        axs[0].set_title("Total Commodity Value ($)")
+        axs[0].set_xlabel("Commodity")
         axs[0].set_ylabel("Value ($)")
 
-        tonnes_data.plot(kind="bar", ax=axs[1], legend=False)  # Plot the graph
-        axs[1].set_title("Total Daily Value (Tonnes)")
-        axs[1].set_xlabel("Day")
+        tonnes_data.plot(kind="bar", ax=axs[1], legend=False)
+        axs[1].set_title("Total Commodity Value (Tonnes)")
+        axs[1].set_xlabel("Commodity")
         axs[1].set_ylabel("Value (Tonnes)")
 
         mngr = plt.get_current_fig_manager()
@@ -430,12 +439,14 @@ class App(customtkinter.CTk):
         data = data.groupby(pd.Grouper(key="Date", freq="M"))["Value"].sum()
         top_5_months = data.sort_values(ascending=False).head(5)
 
-        plt.figure(figsize=(9.39, 6.48))  # Create the bar charts as subplots
+        plt.figure(
+            figsize=(9.39, 6.48), num="5 Most Profitable Months"
+        )  # Create the bar charts as subplots
         plt.bar(top_5_months.index.strftime("%b %Y"), top_5_months.values)
         plt.xlabel("Month", fontsize=14, labelpad=10)
         plt.ylabel("Export Value", fontsize=14, labelpad=20)
         plt.title(
-            "Top 5 Months with Highest Export Value($)",
+            "Top 5 Most Profitable Months",
             fontweight="bold",
             fontsize=16,
             y=1.05,
@@ -494,25 +505,101 @@ class App(customtkinter.CTk):
         # Group data by country and commodity, and calculate total value
         grouped = data.groupby(["Country", "Commodity"])["Value"].sum()
 
-        # Plot the top 6 commodities for each country as subplots in one figure
+        # Plot the top 5 commodities for each country
         countries = grouped.index.levels[0]
         num_plots = len(countries)
         num_rows = 3
-        num_cols = round(num_plots // num_rows)
-        axs = plt.subplots(num_rows, num_cols, figsize=(9.39, 6.48))
+        num_cols = -(-num_plots // num_rows)
+        fig, axs = plt.subplots(
+            num_rows,
+            num_cols,
+            figsize=(9.39, 6.48),
+            num="5 Most Profitable Products",
+        )
         for i, country in enumerate(countries):
             top_commodities = grouped.loc[country].nlargest(5)
             num_commodities = min(6, len(top_commodities))
             top_commodities = top_commodities[:num_commodities]
-            ax = axs[i // num_cols, i % num_cols]
+            row = i // num_cols
+            col = i % num_cols
+            ax = axs[row, col]
             ax.set_title(country)
             ax.bar(top_commodities.index, top_commodities.values)
             ax.tick_params(axis="x", rotation=90)
 
+        fig.suptitle(
+            "Top 5 Most Profitable Products per Country", fontweight="bold", fontsize=16
+        )
         mngr = plt.get_current_fig_manager()
         mngr.window.geometry("+661+210")
         plt.tight_layout()
         plt.show()
+
+    def eighth_figure(self):
+        plt.close("all")
+        usecols = [
+            "Direction",
+            "Date",
+            "Commodity",
+            "Measure",
+            "Value",
+            "Weekday",
+        ]  # Define the Used Columns for this Graph
+        data = pd.read_csv(
+            "data.csv", usecols=usecols
+        )  # Read the .csv file and save it
+        data = data[(data["Direction"] == "Exports") & (data["Measure"] == "$")]
+
+        data = data.replace(
+            [
+                "Milk powder, butter, and cheese",
+                "Meat and edible offal",
+                "Logs, wood, and wood articles",
+                "Fish, crustaceans, and molluscs",
+                "Non-food manufactured goods",
+                "Mechanical machinery and equip",
+                "Electrical machinery and equip",
+            ],
+            [
+                "Dairy",
+                "Meat",
+                "Wood",
+                "Fish",
+                "Other Goods",
+                "Mech Machines",
+                "E-Machines",
+            ],
+        )
+
+        best_days = data.loc[
+            data.groupby("Commodity")["Value"].idxmax()
+        ]  # Grouping while Identifying the max
+
+        num_commodities = len(best_days)  # Number of commodities
+        num_cols = min(num_commodities, 4)
+        num_rows = (num_commodities + num_cols - 1) // num_cols
+        fig, axs = plt.subplots(
+            num_rows,
+            num_cols,
+            figsize=(9.39, 6.48),
+            num="Most Profitable Day per Product",
+        )
+        for i, (commodity, row) in enumerate(best_days.iterrows()):
+            ax = axs[i // num_cols, i % num_cols]
+            ax.bar(row["Weekday"], row["Value"])
+            ax.set_title(row["Commodity"])
+            ax.set_ylabel("Value($)", fontsize=14, labelpad=10)
+
+        # Remove any empty plots
+        for i in range(num_commodities, num_rows * num_cols):
+            axs.flat[i].remove()
+
+        fig.suptitle("Top Days For Each Commodity", fontweight="bold", fontsize=16)
+        mngr = plt.get_current_fig_manager()
+        mngr.window.geometry("+661+210")
+        plt.tight_layout(pad=2.0)
+        plt.show()
+
 
 if __name__ == "__main__":
     app = App()
